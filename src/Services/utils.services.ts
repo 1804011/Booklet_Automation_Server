9855f20b498b19ff82989b03206fb919f318f2b8
Shariful Islam <sharifsharifulislam061@gmail.com>
2024-01-01 19:08:59 +0600

    multer added -cloudinary added -ApiError class and global error handler added -catchAsync defined response interfaces defined role added to prisma model validation handler added

---
+++ src/Services/utils.services.ts
@@ -0,0 +1,33 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import config from '../config'
+
+export const hashPassword = (password: string): string => {
+  const hashedPassword = bcrypt.hashSync(
+    password,
+    Number(config.saltRounds as string),
+  )
+  return hashedPassword
+}
+
+export const comparePasswords = (
+  password: string,
+  hashedPassword: string,
+): boolean => {
+  const match = bcrypt.compareSync(password, hashedPassword)
+  return match
+}
+export const generateToken = (id: string, role: 'teacher' | 'student') => {
+  const token = jwt.sign({ id, role }, config.jwtSecret as string, {
+    expiresIn: '1y',
+  })
+  return token
+}
+export const verifyDate = (date: Date) => {
+  const today = new Date()
+  return (
+    today.getDate() === date.getDate() &&
+    today.getMonth() === date.getMonth() &&
+    today.getFullYear() === date.getFullYear()
+  )
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,9 @@ dotenv.config({
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+  saltRounds: process.env.SALT_ROUND,
+  cloudinaryKey: process.env.CLOUDINARY_KEY,
+  cloudinarySecret: process.env.CLOUDINARY_SECRET,
+  cloudinaryName: process.env.CLOUDINARY_NAME,
 }
